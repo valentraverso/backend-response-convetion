b761c6e030da33ae7a1033b9398505bb4d78e5b2
Valentino Traverso <vt20032003@live.com.ar>
2023-05-04 11:21:25 +0200

    add: 404 response

---
+++ by-html-code/404/response-404.js
@@ -0,0 +1,11 @@
+let res;
+
+// @res is optional
+// other res examples:
+// Express.js: res.status(404).send()
+// PHP: json_encode(array(), 404)
+
+res = {
+    status: false,
+    msg: "We couldn't find results."
+}
